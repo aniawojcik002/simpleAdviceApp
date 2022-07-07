6f27543352e6d5cdac51a982f77af93163db2d0a
ania.wojcik002 <anna.wojcik002@gmail.com>
2022-07-07 23:36:34 +0200

    style: add styles to text

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ async function fetchAdvise() {
   const response = await fetch('https://api.adviceslip.com/advice');
   const data = await response.json()
 
-  adviceP.innerHTML = data.slip.advice;
+  adviceP.innerHTML = `"${data.slip.advice}"`;
   number.innerHTML = data.slip.id;
 }
 
