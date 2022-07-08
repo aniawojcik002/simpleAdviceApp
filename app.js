9dd452f1918ee75f9bff8cc2965c5ad390a7df44
ania.wojcik002 <anna.wojcik002@gmail.com>
2022-07-08 22:54:12 +0200

    Add: event listener to roll dice

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-'use strict'
+"use strict";
 
-const adviceP = document.querySelector('.advice-value');
-const number = document.querySelector('.advice-slip-id')
-
+const adviceP = document.querySelector(".advice-value");
+const number = document.querySelector(".advice-slip-id");
+const rollDice = document.querySelector(".roll-dice-container");
 
 async function fetchAdvise() {
-  const response = await fetch('https://api.adviceslip.com/advice');
-  const data = await response.json()
+  const response = await fetch("https://api.adviceslip.com/advice");
+  const data = await response.json();
 
   adviceP.innerHTML = `"${data.slip.advice}"`;
   number.innerHTML = data.slip.id;
 }
 
-
-fetchAdvise();
+rollDice.addEventListener("click", fetchAdvise);
