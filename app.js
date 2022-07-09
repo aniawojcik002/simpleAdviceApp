8945bf64dc5274ad408e7b68a9d2194ea6c4e565
ania.wojcik002 <anna.wojcik002@gmail.com>
2022-07-09 16:47:31 +0200

    Add loadind spinner, onClick fetching

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
 'use strict'
 
-const adviceP = document.querySelector('.advice-value');
-const number = document.querySelector('.advice-slip-id')
+const adviceP = document.querySelector(".advice-value");
+const number = document.querySelector(".advice-slip-id");
+const rollDice = document.querySelector(".roll-dice-container");
+const loading = document.querySelector('.loading');
 
 
-async function fetchAdvise() {
-  const response = await fetch('https://api.adviceslip.com/advice');
-  const data = await response.json()
-
-  adviceP.innerHTML = `"${data.slip.advice}"`;
-  number.innerHTML = data.slip.id;
+async function fetchAdviceHandler() {
+  try {
+    
+    const response = await fetch("https://api.adviceslip.com/advice", {cache: "no-cache"});
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+    const data = await response.json();
+    // hide loader spiner 
+    loading.classList.add('hidden');
+    // load data to divs
+    adviceP.innerText = `"${data.slip.advice}"`;
+    number.innerText = data.slip.id;
+  }
+  catch(error) {
+    console.error(`Could not get advice: ${error}`);
+  }
 }
 
+function displayLoading() {
+  loading.classList.remove('hidden');
+  adviceP.innerHTML ='';
+}
 
-fetchAdvise();
+rollDice.addEventListener("click", () => {
+  displayLoading();
+  fetchAdviceHandler();
+
+});
